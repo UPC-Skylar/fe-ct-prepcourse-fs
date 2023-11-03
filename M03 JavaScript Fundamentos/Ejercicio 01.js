901d81c1aaeb8d12ab0b7ecbb8339874f92d6f37
UPC-Skylar <zaynml2021@gmail.com>
2023-11-03 11:47:04 -0500

    firstchange

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,7 +5,7 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = null;
+const nuevoString = "Paola";
 
 // Crea una variable de tipo number.
 const nuevoNumero = null;
@@ -24,10 +24,10 @@ const nuevoModulo = 21 % 5 === null;
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
-   nuevoString,
-   nuevoNumero,
-   nuevoBoolean,
-   nuevaResta,
-   nuevaMultiplicacion,
-   nuevoModulo,
+  nuevoString,
+  nuevoNumero,
+  nuevoBoolean,
+  nuevaResta,
+  nuevaMultiplicacion,
+  nuevoModulo,
 };
